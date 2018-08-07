42b64544897d0da5a350b245aa697a1b9b5a1917
Harald Schilly <harald.schilly@gmail.com>
2018-08-07 14:43:47 +0200

    missing bracket (2nd case)

--- snippets_submenus_python/numpy_ufuncs.js
+++ snippets_submenus_python/numpy_ufuncs.js
@@ -907,7 +907,7 @@ define({
                         'axes_a = (2, 0)',
                         'b = np.arange(30).reshape((2,3,5))',
                         'axes_b = (0, 1)',
-                        'np.tensordot(a, b, (axes_a, axes_b)',
+                        'np.tensordot(a, b, (axes_a, axes_b))',
                     ],
                 },
                 {
